17222d65ea319b9e6a67b5c885ab146aaebe5686
tatoclemente <tatoapuros@gmail.com>
2023-07-06 22:46:31 -0300

    deploy en fly.io

--- api/index.js
+++ api/index.js
@@ -21,10 +21,10 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+  server.listen(port, '0.0.0.0', () => {
+    console.log(`%d listening at ${port}`); // eslint-disable-line no-console
   });
 });
