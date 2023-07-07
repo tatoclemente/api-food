623e8aae6d000c1fede91ca3a736a64acc6084ce
tatoclemente <tatoapuros@gmail.com>
2023-07-06 22:58:25 -0300

    solucion de conflictos

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:api/index.js
 //                       _oo0oo_
 //                      o8888888o
 //                      88" . "88
@@ -29,35 +28,3 @@ conn.sync({ force: true }).then(() => {
     console.log(`%d listening at ${port}`); // eslint-disable-line no-console
   });
 });
-=======
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-require('dotenv').config();
-
-const port = process.env.PORT || 3001
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
-  });
-});
->>>>>>> 098bfe6949d5671d7877e2b48db49d1ac89d8892:index.js
